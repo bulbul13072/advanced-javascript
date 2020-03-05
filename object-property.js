ab536d9a66fa9a1684cf9b4e38ccb4c40ee3277e
mrbulbul <you@example.com>
2020-03-05 19:41:31 +0600

    learned a lot

---
+++ object-property.js
@@ -0,0 +1,11 @@
+const students =[
+    {id:21, name: 'Omar Sunny'},
+    {id:31, name: 'Mannaaaaa'},
+    {id:41, name: 'Moyouri'},
+    {id:71, name: 'DeepJol'},
+];
+const names = students.map(s => s.name);
+const iDs = students.map(s=> s.id);
+// const filters = students.filter(s=> s.id > 40);
+const filters = students.find(s=> s.id > 40);
+console.log(filters);
